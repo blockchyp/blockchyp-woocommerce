cc407a686db2e70af1bc107d8d417a612b726b07
Josiah Niedrauer <jniedrauer@gmail.com>
2020-07-24 10:31:03 -0700

    Add deployment automation

---
+++ .github/deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eu
+
+build_dir="$GITHUB_WORKSPACE/build"
+staging_dir="$GITHUB_WORKSPACE/staging"
+git_tag="$GITHUB_REF"
+svn_tag="${git_tag#v}"
+files=(
+    LICENSE
+    blockchyp-woocommerce.php
+    readme.txt
+)
+directories=(
+    assets
+    vendor
+)
+
+rm -rf "$build_dir"
+mkdir "$build_dir"
+rm -rf "$staging_dir"
+
+for f in "${files[@]}"; do
+    cp "$f" "$build_dir/"
+done
+for d in "${directories[@]}"; do
+    cp -r "$d" "$build_dir/$d"
+done
+
+svn checkout http://svn.wp-plugins.org/blockchyp-for-woocommerce "$staging_dir"
+
+test -d "$staging_dir/tags/$svn_tag" && echo "Tag already exists: $svn_tag" && exit 1 || :
+
+rm -rf "$staging_dir/trunk"
+cp -r "$build_dir" "$staging_dir/trunk"
+cp -r "$build_dir" "$staging_dir/tags/$svn_tag"
+
+pushd "$staging_dir"
+svn stat | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
+svn stat | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+
+svn ci --no-auth-cache --username "$SVN_USER" --password "$SVN_PASSWORD" -m "Deploy version $svn_tag"
+popd
--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 ![Build](https://github.com/blockchyp/blockchyp-woocommerce/workflows/Build/badge.svg)
 [![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/blockchyp/blockchyp-woocommerce/blob/master/LICENSE)
+
+## Publishing a new version
+
+Update readme.txt with the new version and changelog.
+
+Create pull request and merge to the main branch.
+
+Tag and push:
+
+```
+git tag -am v1.0.0 v1.0.0
+git push --follow-tags
+```
+
+Check github actions for problems. You should recieve a changelog from
+wordpress within several minutes.

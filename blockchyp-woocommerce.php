629c609a7dcbc38796648731598be4f656076309
Jeffrey Payne <jeff@blockchyp.com>
2020-08-18 11:02:32 -0700

    attempting to fix linting issues

--- blockchyp-woocommerce.php
+++ blockchyp-woocommerce.php
@@ -208,18 +208,16 @@ EOT;
               <input class="blockchyp-input" style="width: 100%;" id="blockchyp_cardholder" name="blockchyp_cardholder"/>
               <input type="hidden" id="blockchyp_token" name="blockchyp_token"/>
             </div>
-            <?php
-                if ($this->settings['render_postalcode'] == 'yes') {
-            ?>
+                <?php
+                    if ($this->settings['render_postalcode'] == 'yes') {
+                ?>
                 <div>
                   <label class="blockchyp-label">Postal Code</label>
                   <input class="blockchyp-input" style="width: 100%;" maxlength="5" id="blockchyp_postalcode" name="blockchyp_postalcode"/>
                 </div>
-             <?php
-                }
-             ?>
+                <?php
+                    } ?>
 <?php ob_end_flush();
-
         }
 
         /**

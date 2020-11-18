7de086b336f551b5ac9cadf5bc2f48d8bce8fb8e
Josiah Niedrauer <josiah@niedrauer.com>
2020-11-18 15:35:48 -0800

    Sync test builds to S3

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -16,36 +16,58 @@ directories=(
     vendor
 )
 
-rm -rf "$build_dir"
-mkdir "$build_dir"
-rm -rf "$staging_dir"
+main() {
+    rm -rf "$build_dir"
+    mkdir "$build_dir"
+    rm -rf "$staging_dir"
 
-for f in "${files[@]}"; do
-    cp "$f" "$build_dir/"
-done
-for d in "${directories[@]}"; do
-    cp -r "$d" "$build_dir/$d"
-done
+    for f in "${files[@]}"; do
+        cp "$f" "$build_dir/"
+    done
+    for d in "${directories[@]}"; do
+        cp -r "$d" "$build_dir/$d"
+    done
 
-svn checkout http://svn.wp-plugins.org/blockchyp-for-woocommerce "$staging_dir"
+    case "${1:-}" in
+        's3') s3_deploy ;;
+        'svn') svn_deploy ;;
+    esac
+}
 
-test -d "$staging_dir/tags/$svn_tag" \
-    && echo "Tag already exists: $svn_tag" && exit 1 \
-    || :
+svn_deploy() {
+    svn checkout http://svn.wp-plugins.org/blockchyp-for-woocommerce "$staging_dir"
 
-rm -rf "$staging_dir/trunk"
-cp -r "$build_dir" "$staging_dir/trunk"
-cp -r "$build_dir" "$staging_dir/tags/$svn_tag"
+    test -d "$staging_dir/tags/$svn_tag" \
+        && echo "Tag already exists: $svn_tag" && exit 1 \
+        || :
 
-rm -rf "$staging_dir/assets"
-cp -r "$build_dir/assets" "$staging_dir/assets"
+    rm -rf "$staging_dir/trunk"
+    cp -r "$build_dir" "$staging_dir/trunk"
+    cp -r "$build_dir" "$staging_dir/tags/$svn_tag"
 
-pushd "$staging_dir"
-svn stat | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
-svn stat | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+    rm -rf "$staging_dir/assets"
+    cp -r "$build_dir/assets" "$staging_dir/assets"
 
-svn ci --no-auth-cache \
-    --username blockchyp \
-    --password "$SVN_PASSWORD" \
-    -m "Deploy version $svn_tag"
-popd
+    pushd "$staging_dir"
+    svn stat | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
+    svn stat | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+
+    svn ci --no-auth-cache \
+        --username blockchyp \
+        --password "$SVN_PASSWORD" \
+        -m "Deploy version $svn_tag"
+    popd
+}
+
+s3_deploy() {
+    aws s3 sync --delete \
+        "$build_dir/" \
+        "s3://$TEST_S3_BUCKET/wordpress/wp-content/plugins/blockchyp-for-woocommerce/"
+
+    aws ecs run-task \
+        --cluster "${WP_CLUSTER}" \
+        --count 1 \
+        --task-definition "${WP_SYNC_TASKDEF}" >/dev/null 2>&1
+}
+
+main "$@"
